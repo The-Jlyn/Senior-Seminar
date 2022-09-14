ae43a8ba513a84a275abde46442170c5868ca00a
The-Jlyn <99376158+The-Jlyn@users.noreply.github.com>
2022-09-14 15:54:52 -0500

    Update main2.cs

--- main2.cs
+++ main2.cs
@@ -1,8 +1,6 @@
 using System;
 
-/* This declaration is a bit different to what we do in Unity,
-but necessary in order to run this live snippet
-*/
+
 class MainClass {
     static void Main() {
         
@@ -12,7 +10,6 @@ class MainClass {
         float result = x + y;
         
         // show the result in the console
-        // in Unity we use "print()" or "Debug.Log()"
         Console.WriteLine(result);
         
         // subtraction
