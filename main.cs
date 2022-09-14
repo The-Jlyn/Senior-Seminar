a88d3d5821c9c73e1bcbf4a6fb2b62bb0876c3e7
The-Jlyn <99376158+The-Jlyn@users.noreply.github.com>
2022-09-14 15:54:03 -0500

    Update main.cs

--- main.cs
+++ main.cs
@@ -1,19 +1,16 @@
 using System;
 
-/* This declaration is a bit different to what we do in Unity,
-but necessary in order to run this live snippet
-*/
+
 class MainClass {
     static void Main() {
         
-        // declaring our variables
+        // declaring variables
         int score = 0;
         float speed = 3.5f;
         bool gameOver = false;
         string item = "Hat";
         
         // show the result in the console
-        // in Unity we use "print()" or "Debug.Log()"
         Console.WriteLine(score);
         Console.WriteLine(speed);
         Console.WriteLine(gameOver);
