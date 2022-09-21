14773223a3111a402fd823102167be3fc6b1b1b6
The-Jlyn <99376158+The-Jlyn@users.noreply.github.com>
2022-09-20 21:58:07 -0500

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+        public float speed;
+    public Vector3 moveDirection;
+    public float moveDistance;
+    private Vector3 startPos;
+    private bool movingToStart;
+    void Start()
+    {
+        startPos = transform.position;
+    }
+    void Update()
+    {
+    if(movingToStart)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, startPos, speed * Time.deltaTime);
+        if(transform.position == startPos)
+        {
+            movingToStart = false;
+        }
+    }
+    else
+    {
+    transform.position = Vector3.MoveTowards(transform.position, startPos + (moveDirection * moveDistance), speed * Time.deltaTime);
+    if(transform.position == startPos + (moveDirection * moveDistance))
+    {
+        movingToStart = true;
+    }
+    }
+    }
+}
