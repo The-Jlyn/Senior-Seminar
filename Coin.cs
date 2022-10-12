05f4222af510728dfbbb6cc6bf6853e57f2ce62a
The-Jlyn <99376158+The-Jlyn@users.noreply.github.com>
2022-10-12 13:35:06 -0500

    Add files via upload

---
+++ Coin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    //How fast is the coin going to be rotating around?
+    public float rotateSpeed;
+    // Update is called once per frame
+    void Update(){
+
+         transform.Rotate(Vector3.up, rotateSpeed * Time.deltaTime);
+    }
+
+
+    private void OnTriggerEnter (Collider other){
+
+        if(other.CompareTag("Player")){
+
+            other.GetComponent<Player>().AddScore(1);
+            Destroy(gameObject);
+        }
+    }
+
+    
+}
